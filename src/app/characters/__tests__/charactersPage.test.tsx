0ca9bf309b54f5a7e0e806f93a6a431e9ba6a1e5
vhlongon <vhlongon@gmail.com>
2024-02-24 23:23:23 +0100

    add test for Characters page

---
+++ src/app/characters/__tests__/charactersPage.test.tsx
@@ -0,0 +1,18 @@
+import { mockPerson } from '@/test/mocks/mockData';
+import { render, screen } from '@testing-library/react';
+import CharactersPage from '../page';
+
+vi.mock('@/utils/session', () => ({
+  getViewedFilms: vi.fn().mockReturnValue([1]),
+}));
+
+describe('Characters page', () => {
+  it('should render main page title and films', async () => {
+    render(await CharactersPage());
+
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Characters viewed in films:'
+    );
+    expect(screen.getByText(mockPerson.name)).toBeInTheDocument();
+  });
+});
