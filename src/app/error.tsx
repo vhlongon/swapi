84ef9c33d8d05c7fd42322cad327194490b2a099
vhlongon <vhlongon@gmail.com>
2024-02-25 23:07:52 +0100

    add error and loading pages

---
+++ src/app/error.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect } from 'react';
+
+type ErrorPageProps = {
+  error: Error & {
+    digest?: string;
+  };
+  reset: () => void;
+};
+
+export default function ErrorPage({ error, reset }: ErrorPageProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="card w-full max-w-96 bg-neutral text-neutral-content">
+        <div className="card-body items-center text-center">
+          <h1 className="card-title text-error">Error</h1>
+          <details className="collapse collapse-arrow border border-base-200 bg-base-100">
+            <summary className="collapse-title font-medium">
+              See details
+            </summary>
+            <div className="collapse-content">
+              <code>{JSON.stringify(error)}</code>
+            </div>
+          </details>
+          <div className="card-actions mt-2 justify-center w-full">
+            <button className="btn btn-outline w-1/2" onClick={reset}>
+              Try again
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default async function Loading() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 gap-4">
+      <span className="loading loading-spinner loading-lg"></span>
+    </main>
+  );
+}
